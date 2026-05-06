a913434fa769395e888eaec6693c05dd3ddc18b8
hakee <keonghak0922@gmail.com>
2026-05-06 15:39:58 +0900

    utils.py에서 함수들을 직접 main.py에서 실행하도록 코드 작성 및 작동 확인

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# 샘플 Python 스크립트입니다.
+from utils import add, sub, mul, div, reverse, is_even
 
-# ⌃R을(를) 눌러 실행하거나 내 코드로 바꿉니다.
-# 클래스, 파일, 도구 창, 액션 및 설정을 어디서나 검색하려면 ⇧ 두 번을(를) 누릅니다.
-
-
-def print_hi(name):
-    # 스크립트를 디버그하려면 하단 코드 줄의 중단점을 사용합니다.
-    print(f'Hi, {name}')  # 중단점을 전환하려면 ⌘F8을(를) 누릅니다.
-
-
-# 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
+print(add(2,4))
+print(sub(2,4))
+print(mul(2,4))
+print(div(2,4))
+print(reverse("안녕하세요"))
+print(is_even(2026))
