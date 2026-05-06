1c07f6f6693d8f0131869fea6b2637c0a18a7aa7
hakee <keonghak0922@gmail.com>
2026-05-06 09:58:19 +0900

    feat: made the codes shorter

---
+++ main.py
@@ -0,0 +1,3 @@
+from utils import reverse, is_even, add, sub, mul, div
+reverse("안녕하세요")
+is_even(2026)
---
+++ utils.py
@@ -0,0 +1,5 @@
+def reverse(str):
+    return str[::-1]
+
+def is_even(n):
+    return n % 2 == 0
